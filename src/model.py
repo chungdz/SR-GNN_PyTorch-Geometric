eda71da5f453ec005d68397b023f50c56cbc3031
Yunfan Hu <1033718596@qq.com>
2020-12-25 13:14:35 +0800

    fifth version

--- src/model.py
+++ src/model.py
@@ -12,11 +12,12 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv, GATConv, GatedGraphConv
 
 class DynamicScore(nn.Module):
-    def __init__(self, hidden_size, n_node):
+    def __init__(self, hidden_size, n_node, layer_num=3):
         super(DynamicScore, self).__init__()
         self.hidden_size = hidden_size
         self.state_lstm = nn.LSTM(self.hidden_size, self.hidden_size)
-        self.residual_lstm = nn.LSTM(self.hidden_size, self.hidden_size)
+        self.layer_num = layer_num
+        self.residual_lstms = nn.ModuleList([nn.LSTM(self.hidden_size, self.hidden_size) for k in range(layer_num)])
         self.norm = nn.LayerNorm(self.hidden_size)
         self.loss = nn.MSELoss()
         # self.embedding = nn.Embedding(n_node, self.hidden_size)
@@ -28,22 +29,27 @@ class DynamicScore(nn.Module):
         seq = seq.permute(1, 0, 2)
         state_seq, (final_state, final_cell) = self.state_lstm(seq)
 
+        cur_seq = state_seq
+        cur_len = seq_len
+        cur_state = final_state
+
+        for l in range(self.layer_num):
+
+            residual_list = []
+            for i in range(1, cur_len):
+                residual_list.append(cur_seq[i] - cur_seq[i - 1])
+            residual = torch.stack(residual_list, dim=0)
+            cur_seq, (final_residual, final_residual_cell) = self.residual_lstms[l](residual)
+            cur_state += final_residual
+            cur_len = cur_len - 1
+
+        next_state = self.norm(cur_state).reshape(-1, self.hidden_size)
+
+        # news loss
         y = y.permute(1, 0, 2)
         # y 1, batch_size, hidden_size
         real_state, (_h, _c) = self.state_lstm(y, (final_state, final_cell))
         real_state = real_state.reshape(-1, self.hidden_size)
-
-        residual_list = []
-        for i in range(1, seq_len):
-            residual_list.append(state_seq[i] - state_seq[i - 1])
-        residual = torch.stack(residual_list, dim=0)
-
-        _seq, (final_residual, _c) = self.residual_lstm(residual)
-
-        next_state = self.norm(final_state + final_residual).reshape(-1, self.hidden_size)
-
-        # Eq(8)
-        # score_2 = torch.mm(next_state, self.embedding.weight.transpose(1, 0))
         
         return next_state, self.loss(next_state, real_state)
 
