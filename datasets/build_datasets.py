d680d9f7b7ab291afae591f169a957dc51304b16
Yunfan Hu <1033718596@qq.com>
2020-12-29 14:25:33 +0800

    build dataset

---
+++ datasets/build_datasets.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""Script for building the training examples.
+
+"""
+import os
+import json
+import random
+import pickle
+import argparse
+import multiprocessing as mp
+from typing import List, Dict
+from numpy.lib.function_base import append
+import math
+from tqdm import tqdm
+import torch
+import pandas as pd
+import numpy as np
+import networkx as nx
+from graph_datasets import MultiSessionsGraph
+from torch_geometric.data import InMemoryDataset, Data
+
+random.seed(7)
+
+def build_subgraph(graph, hist_items, neighbor_items):
+    
+    all_items = hist_items | neighbor_items
+    # nodes 按照 all_ents 的顺序返回和编码
+    all_items = list(all_items)
+    sub_graph = graph.subgraph(all_items)
+
+    impression_ent_id_map = {ent: idx for idx, ent in enumerate(all_items)}
+    sub_graph = nx.relabel_nodes(sub_graph, impression_ent_id_map)
+
+    edges = list(sub_graph.edges)
+    source_nodes, target_nodes = [], []
+    for edge in edges:
+        source_nodes.append(edge[0])
+        target_nodes.append(edge[1])
+    edge_index = [source_nodes, target_nodes]
+
+    hist_mask = [1 if x in hist_items else 0 for x in all_items]
+
+    return all_items, edge_index, hist_mask
+
+def trim_seq(seq, length=30):
+    slen = len(seq)
+
+    if slen >= length:
+        return seq[-length:]
+    else:
+        new_seq = []
+        for t in range(length - slen):
+            new_seq.append(0)
+        return new_seq + seq
+
+def build_examples(rank, args, session_seq, graph, neighbor_dict, output_path):
+    
+    random.seed(7)
+
+    data_list = []
+        
+    for sequences, y in tqdm(zip(session_seq[0], session_seq[1])):
+        item_set = set()
+        cur_len = len(sequences)
+        for i in range(cur_len):
+            item = sequences[cur_len - i - 1]
+            if graph.has_node(item):
+                item_set.add(item)
+            if len(item_set) >= args.node_num:
+                break
+        
+        neighbor_set = set()
+        for item in item_set:
+            neighbor_set.union(neighbor_dict[item])
+
+        x, edge_index, hist_mask = build_subgraph(graph, item_set, neighbor_set)
+        x = torch.LongTensor(x).unsqueeze(1)
+        y = torch.LongTensor([y])
+        edge_index = torch.LongTensor(edge_index)
+        curdata = Data(x=x, edge_index=edge_index, y=y)
+        curdata.sequences = torch.LongTensor(trim_seq(sequences, length=args.seq_len)).unsqueeze(0)
+        curdata.hist_mask = torch.LongTensor(hist_mask).unsqueeze(1)
+        data_list.append(curdata)
+
+    data, slices = MultiSessionsGraph.collate(data_list)
+    torch.save((data, slices), output_path)
+
+
+def main(args):
+    session_seq = pickle.load(open(os.path.join(args.dataset, 'raw', args.filetype + '.txt'), 'rb'))
+    graph = pickle.load(open(os.path.join(args.dataset, 'graph.bin'), 'rb'))
+    neighbor_dict = pickle.load(open(os.path.join(args.dataset, "neighbor.pkl"), 'rb'))
+
+    subdf_len = math.ceil(len(session_seq[0]) / args.processes)
+    cut_indices = [x * subdf_len for x in range(1, args.processes)]
+    session_seqs = np.split(np.array(session_seq[0], dtype=object), cut_indices)
+    session_ys = np.split(session_seq[1], cut_indices)
+    
+    processes = []
+    for i in range(args.processes):
+        output_path = os.path.join(args.dataset, "raw", args.filetype + "-{}.pt".format(i))
+        assert(len(session_seqs[i]) == len(session_ys[i]))
+        p = mp.Process(target=build_examples, args=(i, args, (session_seqs[i], session_ys[i]), graph, neighbor_dict, output_path))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # Path options.
+    parser.add_argument("--filetype", default="train", type=str,
+                        help="train or test")
+    parser.add_argument("--node_num", default=60, type=int,
+                        help="subgraph max node number")
+    parser.add_argument("--seq_len", default=30, type=int,
+                        help="length of sequence")
+    parser.add_argument("--processes", default=10, type=int,
+                        help="Processes number")
+    parser.add_argument("--dataset", default="diginetica", type=str,
+                        help="dataset name")
+
+    args = parser.parse_args()
+
+    main(args)
--- datasets/build_global_graph.py
+++ datasets/build_global_graph.py
@@ -14,40 +14,69 @@ import numpy as np
 import networkx as nx
 
 def build_entire_graph(train_data):
-    edges = set()
+    edges = list()
 
     for sequences, y in zip(train_data[0], train_data[1]):
         cur_len = len(sequences)
         if cur_len < 2:
             continue
         if cur_len == 2:
-            edges.add((sequences[0], sequences[1]))
-            edges.add((sequences[1], sequences[0]))
+            edges.append([sequences[0], sequences[1], 1])
+            edges.append([sequences[1], sequences[0], 1])
         else:
             for i in range(1, cur_len - 1):
-                edges.add((sequences[i], sequences[i - 1]))
-                edges.add((sequences[i - 1], sequences[i]))
+                edges.append([sequences[i], sequences[i - 1], 1])
+                edges.append([sequences[i - 1], sequences[i], 1])
+
+    edge_df = pd.DataFrame(edges, columns=["from", "to", "weight"])
+    edge_weights = edge_df.groupby(["from", "to"]).apply(lambda x: sum(x["weight"]))
+    weighted_edges = edge_weights.to_frame().reset_index().values
 
     dg = nx.DiGraph()
-    dg.add_edges_from(list(edges))
+    dg.add_weighted_edges_from(weighted_edges)
 
     return dg
+
+def build_neighbors_dict(graph, max_neighbor_cnt=5):
+    """
+    For each entity, sampling max_neighbor_cnt neighbors in the entity graph
+    Args:
+        all_ents: all entities in all news
+        graph: the entity graph
+        max_neighbor_cnt:
+
+    Returns:
+        a dict mapping an ent to a set of its neighbors
+    """
+    ent_neighbors_dict = {}
+    all_ents = graph.nodes()
+    for ent in all_ents:
+        # 按照共现次数降序选取邻居
+        sorted_neighbors = sorted(dict(graph[ent]).items(), key=lambda item: -1 * item[1]['weight'])
+        neighbors = [x[0] for x in sorted_neighbors][:max_neighbor_cnt]
+        if len(neighbors) == 0:
+            continue
+        ent_neighbors_dict[ent] = list(set(neighbors))
+
+    return ent_neighbors_dict
 
 
 def main(cfg):
 
     train_data = pickle.load(open(os.path.join(cfg.dataset, 'raw/train.txt'), 'rb'))
+    test_data = pickle.load(open(os.path.join(cfg.dataset, 'raw/test.txt'), 'rb'))
     # Build News Graph
     print("Building Graph")
-    graph = build_entire_graph(train_data)
+    graph = build_entire_graph((train_data[0] + test_data[0], train_data[1] + test_data[1]))
     print("Original Graph built from all items:", len(graph.nodes))
 
     graph.add_node("<pad>")
     graph_path = os.path.join(cfg.dataset, "graph.bin")
     pickle.dump(graph, open(graph_path, 'wb'))
-
-    
-
+    # build neighbor dict
+    ndict = build_neighbors_dict(graph)
+    neighbor_path = os.path.join(cfg.dataset, "neighbor.pkl")
+    pickle.dump(ndict, open(neighbor_path, 'wb'))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
